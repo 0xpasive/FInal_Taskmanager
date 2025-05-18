3f831f0d994c3700793bfd895181b17877c6a7a3
0xpasive <118454575+0xpasive@users.noreply.github.com>
2025-05-18 18:05:58 +0545

    team

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -110,9 +110,9 @@ const TaskForm = ({ teams , onSubmit, onCancel }) => {
                 onChange={handleChange}
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               >
-                <option value="">Select Team</option>
+                
                 {teams
-                  .filter(team => team.createdBy === user.id) // Only show teams created by current user
+                  .filter(team => team.createdBy._id === user.id) // Only show teams created by current user
                   .map(team => (
                     <option key={team._id} value={team._id}>
                       {team.name}
